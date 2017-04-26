9b9f6c2359309e400dea096a1bdb3f81122863fa
lin2280778 <Graz@Mac-Pro-di-Graz.local>
2017-04-26 11:27:11 +0200

    tsetCommit1
    tsetCommit1

---
+++ Pods/Target Support Files/Pods-testGitGit/Pods-testGitGit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testGitGit : NSObject
+@end
+@implementation PodsDummy_Pods_testGitGit
+@end
---
+++ Pods/Target Support Files/Pods-testGitGitTests/Pods-testGitGitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testGitGitTests : NSObject
+@end
+@implementation PodsDummy_Pods_testGitGitTests
+@end
